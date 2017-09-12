38323002e4253d31948efe2a79828e2ce918dc02
Boris Sergeev <bs@4xxi.com>
2017-09-12 17:21:03 +0300

    [Refactor] split "add" logic into smaller functions

--- src/StringCalculator.elm
+++ src/StringCalculator.elm
@@ -3,7 +3,10 @@ module StringCalculator exposing (add)
 add : String -> Int
 add input =
   if String.isEmpty input then 0
-  else String.split "," input
-    |> List.map String.toInt
-    |> List.map (Result.withDefault 0)
-    |> List.sum
+  else splitInput >> parseNumbers >> List.sum <| input
+
+splitInput : String -> List String
+splitInput = String.split ","
+
+parseNumbers : List String -> List Int
+parseNumbers = List.map <| Result.withDefault 0 << String.toInt
