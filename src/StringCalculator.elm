b43035a19b980050734d121d4a1e2892aa203243
Boris Sergeev <bs@4xxi.com>
2017-09-12 17:33:42 +0300

    [Green] handling of new lines in "add" implementation

--- src/StringCalculator.elm
+++ src/StringCalculator.elm
@@ -1,12 +1,14 @@
 module StringCalculator exposing (add)
 
+import Regex
+
 add : String -> Int
 add input =
   if String.isEmpty input then 0
   else splitInput >> parseNumbers >> List.sum <| input
 
 splitInput : String -> List String
-splitInput = String.split ","
+splitInput = Regex.split Regex.All <| Regex.regex ",|\\n"
 
 parseNumbers : List String -> List Int
 parseNumbers = List.map <| Result.withDefault 0 << String.toInt
