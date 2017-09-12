8b29ed87dba7714923323dcd8a76ddd297faa3cf
Boris Sergeev <bs@4xxi.com>
2017-09-12 17:10:22 +0300

    [Green] reference implementation of "add" for multiple numbers

--- src/StringCalculator.elm
+++ src/StringCalculator.elm
@@ -3,4 +3,7 @@ module StringCalculator exposing (add)
 add : String -> Int
 add input =
   if String.isEmpty input then 0
-  else String.toInt input |> Result.withDefault 0
+  else String.split "," input
+    |> List.map String.toInt
+    |> List.map (Result.withDefault 0)
+    |> List.sum
