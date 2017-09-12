4b7a405c96f3a4767622576cb3a3308914a35dbd
Boris Sergeev <bs@4xxi.com>
2017-09-12 16:53:36 +0300

    [Red] test "add" with string of one number

--- tests/StringCalculatorTests.elm
+++ tests/StringCalculatorTests.elm
@@ -9,6 +9,14 @@ suite : Test
 suite =
   describe "StringCalculator"
     [ describe "\"add\" method"
+
+
       [ test "can be called with empty string" <|
         \_ -> Expect.equal (StringCalculator.add "") 0
+
+
+      , test "can be called with string of one number" <|
+        \_ -> Expect.equal (StringCalculator.add "123") 123
+
+
       ]]
