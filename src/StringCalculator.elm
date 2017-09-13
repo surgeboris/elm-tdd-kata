ddbcfaf8f6484aee0e5b12e8c1888696521aaaec
Boris Sergeev <bs@4xxi.com>
2017-09-13 11:00:01 +0300

    [Green] logic for custom delimiter in "add"

--- src/StringCalculator.elm
+++ src/StringCalculator.elm
@@ -14,8 +14,8 @@ add input =
 splitInput : String -> Result AdditionError (List String)
 splitInput input =
   let
-    delimiter = Regex.regex ",|\\n"
-    splitList = Regex.split Regex.All delimiter input
+    { delimiter, strippedInput } = getDelimiter input
+    splitList = Regex.split Regex.All delimiter strippedInput
     filterEmptyStrings = List.filter <| not << String.isEmpty
     lastSplitItem = (List.head << List.reverse) splitList
   in
@@ -25,6 +25,32 @@ splitInput input =
           then Err "Wrong input: superfluous delimiter at the end"
         else Ok <| filterEmptyStrings splitList
       Nothing -> Err "Program error: splitInput cannot get last split item!"
+
+type alias DelimiterData = {
+  delimiter : Regex.Regex,
+  strippedInput : String
+}
+getDelimiter : String -> DelimiterData
+getDelimiter input =
+  let
+    customDelimiter = Regex.regex "^//(.{1})\\n"
+    defaultDelimiter = Regex.regex ",|\\n"
+    pickCustomDelimiter =
+      Regex.find (Regex.AtMost 1) customDelimiter
+      >> List.map .submatches >> List.concat
+      >> List.head >> Maybe.withDefault Nothing
+    stripInput =
+      Regex.replace (Regex.AtMost 1) customDelimiter <| always ""
+  in
+    case pickCustomDelimiter input of
+      Nothing -> {
+        delimiter = defaultDelimiter,
+        strippedInput = input
+      }
+      Just r -> {
+        delimiter = Regex.regex <| Regex.escape r,
+        strippedInput = stripInput input
+      }
 
 appendItemIntoList : a -> List a -> List a
 appendItemIntoList item list = list ++ [item]
